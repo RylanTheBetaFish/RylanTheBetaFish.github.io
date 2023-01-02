517b35f4deefc20dc8152dc49a76846caa0a0e90
RylanTheBetaFish <rylanholverson@gmail.com>
2023-01-01 21:28:58 -0700

    betafish 1.0

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+// interactive hero scripts
+let cursor = document.getElementById('cursor');
+window.onmousemove = function(e){
+    cursor.style.left = e.clientX + 'px'
+    cursor.style.top = e.clientY + 'px'
+}

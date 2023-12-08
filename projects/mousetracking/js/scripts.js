447c62552cbb69d1f673fa6fc4712b9ff3f9e4c2
RylanTheBetaFish <rylanholverson@gmail.com>
2023-12-08 02:40:38 -0700

    Update Featured Projects
    swap the original mousetracking for a calculator

--- projects/mousetracking/js/scripts.js
+++
@@ -1,6 +0,0 @@
-// interactive hero scripts
-let cursor = document.getElementById('cursor');
-window.onmousemove = function(e){
-    cursor.style.left = e.clientX + 'px'
-    cursor.style.top = e.clientY + 'px'
-}
